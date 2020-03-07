3ef141a27055fcdfd9077b7d0b7592d7d254ca05
Jozef Mravec <jojomravec@gmail.com>
2020-03-07 18:58:20 +0100

    Changes

--- QuizPlus/QuizPlus/MainPage.xaml.cs
+++ QuizPlus/QuizPlus/MainPage.xaml.cs
@@ -13,13 +13,36 @@ namespace QuizPlus
     [DesignTimeVisible(false)]
     public partial class MainPage : ContentPage
     {
+        private List<string> Countries = new List<string>()
+        {
+            "armensko","grecko","holandsko","lotssko","slovensko","maďarsko","ukrajina","poľsko"
+        };
+        private Button[] Buttons;
+        private int buttonText;
+
         public MainPage()
         {
             InitializeComponent();
-private void Button_Clicked(object sender, EventArgs e)
+            Buttons = new[] { b1, b2, b3, b4 };
+            GetRightContry();
+        }
+
+        private void Clicked(object sender, EventArgs e)
         {
+            //Navigation.PushAsync(new Page1());
+            GetRightContry();
+        }
+
+        private void GetRightContry()
+        {
+            var random = new Random();
+            var i = random.Next(0, Countries.Count - 1);
+            var country = Countries[i];
+            countryname.Text = country;
+            countryflag.Source = $"{country}.png";
+            buttonText = random.Next(0, 3);
+            Buttons[buttonText].Text = country;
 
         }
-    }
     }
 }
